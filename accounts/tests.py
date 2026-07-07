569f1bab3c5fa33a87244a22677c4c4176bddfa3
Abdullah Hodan <abdullahhodan448@gmail.com>
2026-07-07 16:40:36 +0600

    admin.py fixed because of password not hassing

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class TeacherRegistrationTests(TestCase):
+    def test_teacher_registration_page_redirects_to_student_registration(self):
+        response = self.client.get(reverse('teacher_register'))
+
+        self.assertRedirects(response, reverse('student_register'))
+
+    def test_teacher_registration_post_redirects_to_student_registration(self):
+        response = self.client.post(
+            reverse('teacher_register'),
+            data={
+                'username': 'teacheruser',
+                'email': 'teacher@example.com',
+                'password1': 'StrongPass123',
+                'password2': 'StrongPass123',
+            },
+        )
+
+        self.assertRedirects(response, reverse('student_register'))
